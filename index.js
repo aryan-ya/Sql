dcd195d002b0046c95d9950ac7b7ce740e08bb89
aryan-ya <ay3978943@gmail.com>
2024-09-28 16:13:17 +0530

    add index

---
+++ index.js
@@ -0,0 +1,16 @@
+// CJS
+const { faker } = require('@faker-js/faker');
+
+
+export function createRandomUser() {
+    return {
+      userId: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    };
+  }
+  
