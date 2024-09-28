c5381893c7682c7044384c5e29c625b9c3a46d60
aryan-ya <ay3978943@gmail.com>
2024-09-28 18:10:09 +0530

    learn

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-// CJS
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
+// Creating a connection to the database
+const connection  = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'Aryan456@234'
+});
 
-export function createRandomUser() {
-    return {
-      userId: faker.string.uuid(),
-      username: faker.internet.userName(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      password: faker.internet.password(),
-      birthdate: faker.date.birthdate(),
-      registeredAt: faker.date.past(),
-    };
-  }
-  
+// Connect to the database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database.');
+
+    // Prepare the SQL query for bulk insertion
+    let q  = "INSERT INTO user(id, username, email, password) VALUES ?";
+    
+    // Generate random users using faker
+    let users = [
+        [faker.string.uuid(), faker.internet.userName(), faker.internet.email(), faker.internet.password()],
+        [faker.string.uuid(), faker.internet.userName(), faker.internet.email(), faker.internet.password()],
+    ];
+
+    // Insert the users into the database
+    connection.query(q, [users], (err, result) => {
+        if (err) {
+            console.error("Error inserting data:", err);
+            return;
+        }
+        console.log(result);
+    });
+
+    // Close the connection after the query
+    connection.end();
+});
